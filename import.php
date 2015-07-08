53ada4063e58e96ecdae28c48a93af01494b41de
wang-tom <wanggang20120401@gmail.com>
2015-07-08 11:38:20 +0800

    Create import.php

---
+++ import.php
@@ -0,0 +1,37 @@
+<?php
+header("Content-type:text/html;charset=utf-8");
+
+include_once 'phpExcelIO.php';
+$excelIO = new phpExcelIO();
+
+@$fileInfo=$_FILES['myFile'];
+if(isset($fileInfo)){
+	$allowExt=array('txt','xls');
+	$newName = $excelIO->uploadFile($fileInfo,'upload',false,$allowExt);
+	
+	$csv_arr = array();
+	$result = array();
+	if (file_exists($newName)){
+		$csv_arr = $excelIO->excelIOManage($newName);
+		
+		foreach ($csv_arr as $key=>$value) {
+			$createtime = time() + $key;
+			$imgarr_color = $excelIO->get_color($csv_arr[$key]['desc']);
+			$imgarray = $excelIO->get_imgarr($imgarr_color);
+			$goods_spec = $excelIO->get_goodsdesc($imgarray, $createtime);
+			var_dump($imgarray);
+			$count_color = count($imgarray);
+			if($count_color > 0){
+				for ($i=0;$i<$count_color;$i++){
+					$pro_desc = $excelIO->get_prodesc($imgarray,$createtime,$i);
+					
+				}
+			}
+			//var_dump($spec);
+		}		
+		
+		//echo '<script>window.location.href="admin.php"</script>';
+	}else{
+		echo "upload error";
+	}
+}
